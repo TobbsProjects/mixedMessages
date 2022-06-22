ed8db709fa0c502fd5c5f43e7830ade1b2540562
Tobias Östh <tobsson89@gmail.com>
2022-06-22 10:54:06 +0200

    Basic little app now

--- main.js
+++ main.js
@@ -1,15 +1,13 @@
 
 
 function mixedMessage() {
-    const verb = ['Yellow', 'Submarine', 'Boat', 'Fisherman'];
-    const subject = ['Fish', 'Joker', 'Trilliad', 'Mishmash'];
-    const static = ['Potatoes', 'Sallad', 'Tomatoes', 'Glorious'];
+    const subject = ['A man', 'You', 'I', 'He', 'She', 'A woman', 'The kid', 'Your sister', 'Your mom', 'Your dog', 'A fish', 'A dog', 'An ungreatful bastard', 'The hobo'];
+    const verb = ['missed', 'came late to', 'walked', 'smashed', 'played', 'DJ\'d', 'ran', 'ran to', 'hurried to', 'slowmo-walked', 'snailed', 'went'];
+    const object = ['the stripclub', 'the library', 'the ally', 'the kitchen', 'the woods', 'your sister', 'your mom', 'your dog', 'a fish', 'your brother', 'your daddy', 'the shit', 'the bathroom', 'the nasty booth'];
     //Assign output with three words
-    const wordOne = verb[Math.floor(Math.random()*4)];
-    const wordTwo = subject[Math.floor(Math.random()*4)];
-    const wordThree = static[Math.floor(Math.random()*4)];
+    const wordOne = subject[Math.floor(Math.random()*14)];
+    const wordTwo = verb[Math.floor(Math.random()*12)];
+    const wordThree = object[Math.floor(Math.random()*14)];
+    document.getElementById("message").innerHTML = `${wordOne} ${wordTwo} ${wordThree}`;
 
-    return `${wordOne} ${wordTwo} ${wordThree}`
 }
-
-console.log(mixedMessage());
